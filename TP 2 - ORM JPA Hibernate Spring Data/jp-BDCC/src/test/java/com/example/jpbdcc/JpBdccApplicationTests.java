5c9a579e43b2f6bb8b926b7080a633758e1fe3ee
Reda Mitar <rmitar@OMNISHORE333.onmicrosoft.com>
2024-03-07 10:26:44 +0100

    Activité Pratique N°2 - ORM, JPA Hibernate Spring Data

---
+++ TP 2 - ORM JPA Hibernate Spring Data/jp-BDCC/src/test/java/com/example/jpbdcc/JpBdccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpbdcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpBdccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
